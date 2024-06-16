23de780309cc41c45477e539ac0850558df66ef6
intanaprilia04 <apriliaintan062@gamil.com>
2024-06-16 23:35:46 +0700

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\View\View;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listening of the resource
+     * 
+     * @return View
+     */
+    public function index(): View
+    {
+        // Kirim data post ke view
+        return view('posts.index');
+    }
+
+    public function view(){
+        return view ('posts.view');
+    }
+    public function edit(){
+        return view ('posts.edit');
+    }  
+    public function login(){
+        return view ('posts.login');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\View\View;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('posts.index');
+});
+// Route untuk resource post
+Route::resource('/posts', PostController::class);
+
+Route::get('/view', [PostController::class, 'view'])->name('posts.view');
+Route::get('/edit', [PostController::class, 'view'])->name('posts.edit');
+Route::get('/view', [PostController::class, 'view'])->name('posts.login');
+
+
